d22788e3089f6526c0889688c92dd6106891505a
Maryam <maryaada@student.42abudhabi.ae>
2025-12-15 11:53:57 +0400

    added ps header

---
+++ pushswap fol/push_swap/push_swap.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maryaada <maryaada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/10 15:20:43 by maryaada          #+#    #+#             */
+/*   Updated: 2025/12/10 18:13:17 by maryaada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h> //remove this at some point
+
+typedef struct stack_list {
+	int data;
+	void *next;
+} s_list;
+
+void	ft_lstadd_back(s_list **lst, s_list *new);
+s_list	*ft_lstnew(int data);
+int	mini_atoi(char *str);
+
+#endif
