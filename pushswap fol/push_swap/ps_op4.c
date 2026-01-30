4a8c898d02ee233ee979b307134a6a92bfd341b4
Maryam Adam <maryaada@lab2r3s14.42abudhabi.ae>
2026-01-30 16:26:21 +0400

    push

--- pushswap fol/push_swap/ps_op4.c
+++ pushswap fol/push_swap/ps_op4.c
@@ -1,3 +1,17 @@
 #include "push_swap.h"
 
 //push a and push b operations
+
+//push a (push from b to a)
+void	ps_pa(s_list **stack_list_a, s_list **stack_list_b)
+{
+	s_list	*temp;
+
+	if (!*stack_list_b)
+		return ;
+	temp = *stack_list_b;
+	*stack_list_b = (*stack_list_b)->next;
+	temp->next = *stack_list_a;
+	*stack_list_a = temp;
+	write(1, "pa\n", 3);	
+}
