162dc13bf1995c1457da01d1f731473b7e01490b
Maryam Adam <maryaada@lab2r3s14.42abudhabi.ae>
2026-02-07 20:54:32 +0400

    main

--- pushswap fol/push_swap/main.c
+++ pushswap fol/push_swap/main.c
@@ -1,123 +1,127 @@
+
+
+//=============================
+
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maryaada <maryaada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 19:47:09 by maryaada          #+#    #+#             */
+/*   Updated: 2026/02/07 19:54:02 by maryaada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
+
+void	free_stack(s_list *stack)
+{
+    s_list	*temp;
+
+    while (stack)
+    {
+        temp = stack->next;
+        free(stack);
+        stack = temp;
+    }
+}
+
+void	push_swap(s_list **stack_a, s_list **stack_b)
+{
+    int	size;
+
+    size = ft_listsize(*stack_a);
+
+    if (size == 2)
+    {
+        sort_two(stack_a);
+        return ;
+    }
+    if (size == 3)
+    {
+        sort_three(stack_a);
+        return ;
+    }
+	reset_rank(stack_a);
+    node_rank(stack_a);
+
+    while (ft_listsize(*stack_a) > 3)
+        ps_pb(stack_a, stack_b);
+    
+    sort_three(stack_a);
+    
+    while (*stack_b)
+    {
+        set_pos(*stack_a);
+        set_pos(*stack_b);
+        calculate_costs(*stack_a, *stack_b);
+        execute_moves(stack_a, stack_b, find_cheapest(*stack_b));
+    }
+
+	set_pos(*stack_a);
+    while ((*stack_a)->rank != 0)
+    {
+        if ((*stack_a)->pos <= ft_listsize(*stack_a) / 2)
+            ps_ra(stack_a);
+        else
+            ps_rra(stack_a);
+        set_pos(*stack_a);
+    }
+}
+
+int	parse_input(int argc, char **argv, s_list **stack_a)
+{
+	int	i;
+	int	num;
+	int	over_check;
+
+	i = 1;
+	over_check = 0;
+	while (i < argc - 1)
+	{
+		if (!arg_is_num(argv[i]))
+		{
+			write(2, "Error\n", 6);
+			return (0);
+		}
+		num = to_num(argv[i], over_check);
+		if (arg_is_dup(*stack_a))
+		{
+			write(2, "Error\n", 6);
+			return (0);
+		}
+		add_node(stack_a, num);
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
-	if (argc > 2)
+    s_list	*stack_a;
+    s_list	*stack_b;
+
+    stack_a = NULL;
+    stack_b = NULL;
+    
+    // Parse input and build stack_a
+    // ...your parsing code...
+	if (argc < 2)
 	{
-		int i = 2; 
-		s_list *stack_a = NULL;
-
-		int num_flag = 0;
-		stack_a = ft_listnew(to_num(argv[1], &num_flag));
-		s_list *loop_list = stack_a; //loop through the list safely without losing pointer to head
-		while (i < (argc))
-		{
-			if (arg_is_dup(argv) == 1 || arg_is_num(argv) == 1)
-			{
-				return (write(2, "Error\n", 6), 1);
-			}
-			ft_listadd_back(&loop_list, ft_listnew(to_num(argv[i], &num_flag)));
-			if (num_flag == 1)
-			{
-				write(2, "Error\n", 6);
-				return(1);
-			}
-			loop_list = loop_list->next;
-			i++;
-		}
-		s_list *head_ptr = stack_a;
-		while(head_ptr != NULL)  //loop and print list before operations
-		{
-			printf("list has %d\n", head_ptr->data);
-			head_ptr = head_ptr->next;
-		}
-
-		//make stack b
-		// s_list	*stack_b;
-		// stack_b = ft_listnew(5);
-		// stack_b->next = ft_listnew(9);
-		// s_list	*b_head = stack_b; //head of stack_b
-
-		// printf("\n");
-		// while(b_head != NULL)  //loop through b
-		// {
-		// 	printf("blist has %d\n", b_head->data);
-		// 	b_head = b_head->next;
-		// }
-
-		/*  operation testing */
-		if (stack_a)
-		{
-			printf("------after operations \n");
-			/* ps_ra(&stack_a);
-			ps_rb(&stack_b); */
-			// ps_rr(&stack_a, & stack_b);
-			// ps_pb(&stack_a, &stack_b);
-			s_list *op_a = stack_a;
-			if (argc == 4)
-				sort_three(&op_a);
-			if (argc == 3)
-				sort_two(&op_a);
-			
-			printf("min number is %d\n", min_num(op_a)->data);
-
-			while(op_a != NULL)  //loop and print list after operations
-			{
-				printf("list has %d\n", op_a->data);
-				op_a = op_a->next;
-			}
-			// printf("\n");
-			// b_head = stack_b;
-			// while(b_head != NULL)  //loop through b
-			// {
-			// 	printf("blist has %d\n", b_head->data);
-			// 	b_head = b_head->next;
-			// }
-		}
+		write(2, "Error\n", 6);
+		return (1);
+	}
+    else
+		parse_input(argc, argv, &stack_a);
 	
-	}
-	else if (argc == 2 && is_it_space(argv) == 0)
-	{
-		int w;
-		// if (has_space(argv) == 1)
-		// {
-		// 	if (arg_is_dup(argv) == 1 || arg_overflow(argv) == 1)
-		// 	{
-		// 		return (write(2, "Error\n", 6), 1);
-		// 	}
-		// }
-		char **split_num = ft_split(argv[1], ' ');
-		s_list *stack_a = NULL;
+    push_swap(&stack_a, &stack_b);
 
-		int num_flag = 0;
-		stack_a = ft_listnew(to_num(split_num[0], &num_flag));
-		s_list *loop_list = stack_a; //link to list head
-		w = 1;
-		while (split_num[w] != NULL)
-		{
-			if (arg_is_dup(argv) == 1 || arg_is_num(argv) == 1)
-			{
-				return (write(2, "Error\n", 6), 1);
-			}
-			ft_listadd_back(&loop_list, ft_listnew(to_num(split_num[w], &num_flag)));
-			if (num_flag == 1)
-			{
-				write(2, "Error\n", 6);
-				return(1);
-			}
-			loop_list = loop_list->next;
-			w++;
-		}
-		s_list *head_ptr = stack_a; // to reset the position back to list head
-		while(head_ptr != NULL)
-		{
-			printf("-sep list preview after split: %d\n", head_ptr->data);
-			head_ptr = head_ptr->next;
-		}
-	}
-	else
-	{
-		// printf("idk something is wrong with the code up there mate figure it out\n");
-		return (err_ret(), 1);
-	}
+	free_stack(stack_a);
+	free_stack(stack_b);
+    
+    return (0);
 }
+
+
