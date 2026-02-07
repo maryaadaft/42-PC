7a74d946abb5f80729ccda1b6216015a9e439841
Maryam Adam <maryaada@lab2r3s14.42abudhabi.ae>
2026-02-07 19:37:12 +0400

    sort2

--- pushswap fol/push_swap/ps_sort_utils.c
+++ pushswap fol/push_swap/ps_sort_utils.c
@@ -82,5 +82,56 @@ void	node_rank(s_list **stack)
 
 s_list	*target_in_a(s_list *stack_a, s_list *stack_b)
 {
-	
+	s_list	*target;
+    s_list	*current;
+
+    target = NULL;
+    current = stack_a;
+    while (current)
+    {
+        if (current->rank > stack_b->rank && 
+            (!target || current->rank < target->rank))
+            target = current;
+        current = current->next;
+    }
+    if (!target)
+        target = min_num(stack_a);
+    return (target);
 }
+
+void	calculate_costs(s_list *stack_a, s_list *stack_b)
+{
+    s_list	*current_b;
+    s_list	*target;
+    int		cost_a;
+    int		cost_b;
+
+    current_b = stack_b;
+    while (current_b)
+    {
+        target = target_in_a(stack_a, current_b);
+        cost_a = get_cost(target->pos, ft_listsize(stack_a));
+        cost_b = get_cost(current_b->pos, ft_listsize(stack_b));
+        current_b->cost_a = cost_a;
+        current_b->cost_b = cost_b;
+        current_b->total_cost = abs(cost_a) + abs(cost_b);
+        current_b = current_b->next;
+    }
+}
+
+s_list	*find_cheapest(s_list *stack_b)
+{
+    s_list	*cheapest;
+    s_list	*current;
+
+    cheapest = stack_b;
+    current = stack_b->next;
+    while (current)
+    {
+        if (current->total_cost < cheapest->total_cost)
+            cheapest = current;
+        current = current->next;
+    }
+    return (cheapest);
+}
+
